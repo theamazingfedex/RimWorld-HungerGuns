f2cb540cadee3d190610564a022a2b452e001e34
Daniel Wood <daniel.wood4@extendhealth.com>
2019-06-19 21:23:02 -0600

    Add the hunger rifle

--- Source/HungerGuns.cs
+++ Source/HungerGuns.cs
@@ -3,53 +3,34 @@ using Verse;
 
 namespace HungerGuns
 {
-    public class ThingDef_HungerBullet : ThingDef
-    {
-        public float AddHediffChance = 0.9f;
-    }
-
     public class Projectile_HungerBullet : Bullet
     {
-        #region Properties
-        //
-        public ThingDef_HungerBullet Def
-        {
-            get
-            {
-                return this.def as ThingDef_HungerBullet;
-            }
-        }
-        #endregion Properties
-
         #region Overrides
         protected override void Impact(Thing hitThing)
         {
             base.Impact(hitThing);
 
-            if (Def != null && hitThing != null && hitThing is Pawn hitPawn)
+            if (def != null && hitThing != null && hitThing is Pawn hitPawn)
             {
                 var rand = Rand.Value;
-                if (rand <= Def.AddHediffChance)
+                if (rand <= 1f && !hitPawn.health.hediffSet.HasHediff(HediffDefOf.Malnutrition))
                 {
                     Messages.Message("Bullet_HungerBullet_SuccessMessage".Translate(
-                        this.launcher.Label, hitPawn.Name
+                        this.launcher.Label
                     ), MessageTypeDefOf.NeutralEvent);
 
                     Need_Food foodNeed = hitPawn.needs.food;
                     foodNeed.CurLevelPercentage = 0;
                     foodNeed.CurLevel = 0;
                     hitPawn.needs.food = foodNeed;
-                    if (!hitPawn.health.hediffSet.HasHediff(HediffDefOf.Malnutrition))
-                    {
-                        hitPawn?.health?.AddHediff(HediffDefOf.Malnutrition);
-                    }
+                    hitPawn?.health?.AddHediff(HediffDefOf.Malnutrition);
 
                     hitPawn?.mindState?.priorityWork?.ClearPrioritizedWorkAndJobQueue();
                     hitPawn?.mindState?.mentalStateHandler?.TryStartMentalState(DefDatabase<MentalStateDef>.GetNamed("Binging_Food"));
                 }
                 else
                 {
-                    MoteMaker.ThrowText(hitThing.PositionHeld.ToVector3(), hitThing.MapHeld, "Bullet_HungerBullet_FailureMote".Translate(Def.AddHediffChance), 12f);
+                    MoteMaker.ThrowText(hitThing.PositionHeld.ToVector3(), hitThing.MapHeld, "Bullet_HungerBullet_FailureMote".Translate(0), 12f);
                 }
             }
         }
